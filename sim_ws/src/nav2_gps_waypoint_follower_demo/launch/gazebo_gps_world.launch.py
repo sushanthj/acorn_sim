26d99d537c0481740544b1289e01322cc58ffd09
sushanth <sushanth.jayanth@gmail.com>
2024-08-09 14:28:42 -0400

    trying to add acron instead of turtlebot

--- sim_ws/src/nav2_gps_waypoint_follower_demo/launch/gazebo_gps_world.launch.py
+++ sim_ws/src/nav2_gps_waypoint_follower_demo/launch/gazebo_gps_world.launch.py
@@ -13,42 +13,40 @@
 # limitations under the License.
 
 import os
+import xacro
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-
 
 def generate_launch_description():
     # Get the launch directory
-    gps_wpf_dir = get_package_share_directory(
-        "nav2_gps_waypoint_follower_demo")
+    # Get the launch directory
+    gps_wpf_dir = get_package_share_directory("nav2_gps_waypoint_follower_demo")
     launch_dir = os.path.join(gps_wpf_dir, 'launch')
     world = os.path.join(gps_wpf_dir, "worlds", "sonoma_raceway.world")
 
-    # urdf = os.path.join(gps_wpf_dir, 'urdf', 'turtlebot3_waffle_gps.urdf')
-    urdf = os.path.join(gps_wpf_dir, 'urdf', 'robot.urdf')
-    with open(urdf, 'r') as infp:
-        robot_description = infp.read()
+    xacro_file = os.path.join(gps_wpf_dir, 'urdf', 'robot_sim.urdf.xacro')
+
+    # Validate the Xacro file using the --check option
+    validate_xacro = ExecuteProcess(
+        cmd=['ros2', 'run', 'xacro', 'xacro', '--check', xacro_file],
+        output='screen'
+    )
+
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = robot_description_config.toxml()
 
     models_dir = os.path.join(gps_wpf_dir, "models")
-    models_dir += os.pathsep + \
-        f"/opt/ros/{os.getenv('ROS_DISTRO')}/share/turtlebot3_gazebo/models"
-    set_gazebo_model_path_cmd = None
 
-    if 'GAZEBO_MODEL_PATH' in os.environ:
-        gazebo_model_path = os.environ['GAZEBO_MODEL_PATH'] + \
-            os.pathsep + models_dir
-        set_gazebo_model_path_cmd = SetEnvironmentVariable(
-            "GAZEBO_MODEL_PATH", gazebo_model_path)
-    else:
-        set_gazebo_model_path_cmd = SetEnvironmentVariable(
-            "GAZEBO_MODEL_PATH", models_dir)
+    set_gazebo_model_path_cmd = SetEnvironmentVariable(
+        "GAZEBO_MODEL_PATH", models_dir)
 
-    set_tb3_model_cmd = SetEnvironmentVariable("TURTLEBOT3_MODEL", "waffle")
-
+    # Alternative to launch gazebo server and client
+    """
     # Specify the actions
     start_gazebo_server_cmd = ExecuteProcess(
         cmd=['gzserver', '-s', 'libgazebo_ros_init.so',
@@ -59,25 +57,75 @@ def generate_launch_description():
         cmd=['gzclient'],
         cwd=[launch_dir], output='both')
 
+    # Launch Gazebo, load the world
+    start_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')]),
+        launch_arguments={'world': world_file}.items(),
+    )
+
+    # Launch the Gazebo client (optional, for GUI)
+    start_gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')]),
+    )
+    """
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={
+            'world': world,
+            'verbose': 'true',
+        }.items()
+    )
+
+    # Spawning the robot
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', "acorn", '-topic', '/robot_description'],
+        output='screen'
+    )
+
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        output='both',
-        parameters=[{'robot_description': robot_description}])
+        output='screen',
+        parameters=[{'robot_description': robot_description, 'use_sim_time': True}]
+    )
+
+    start_joint_state_publisher_cmd = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+    )
+
+    # Starting the teleop node
+    teleop = Node(
+        package='teleop_twist_keyboard',
+        executable="teleop_twist_keyboard",
+        output='screen',
+        prefix='xterm -e',
+        name='teleop'
+    )
 
     # Create the launch description and populate
     ld = LaunchDescription()
 
-    # Set gazebo up to find models properly
-    ld.add_action(set_gazebo_model_path_cmd)
-    ld.add_action(set_tb3_model_cmd)
+    # validate xacro
+    ld.add_action(validate_xacro)
 
-    # simulator launch
-    ld.add_action(start_gazebo_server_cmd)
-    ld.add_action(start_gazebo_client_cmd)
+    # Launch Gazebo
+    ld.add_action(set_gazebo_model_path_cmd)
+    ld.add_action(gazebo)
 
     # robot state publisher launch
     ld.add_action(start_robot_state_publisher_cmd)
+    ld.add_action(spawn_entity)
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(teleop)
 
     return ld
